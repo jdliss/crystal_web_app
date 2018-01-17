bd6fe04bb13fb7ed526a0f7d29e212d33eb1cf45
Jonathan Liss <jonacom@lissismore.com>
2018-01-17 13:37:28 -0500

    move kemal config to top

--- src/crystal_web_app.cr
+++ src/crystal_web_app.cr
@@ -2,11 +2,11 @@ require "./crystal_web_app/*"
 require "kemal"
 
 module CrystalWebApp
+  Kemal.config.port = 3000
 
   get "/" do
     render "src/crystal_web_app/views/home.ecr", "src/crystal_web_app/views/layouts/main.ecr"
   end
 
-  Kemal.config.port = 3000
   Kemal.run
 end
