31b6911cd6094d3ea14a3da4b2c9daffa31afbee
Jonathan Liss <jonacom@lissismore.com>
2018-01-17 13:58:33 -0500

    add explanation of render

--- src/crystal_web_app.cr
+++ src/crystal_web_app.cr
@@ -5,6 +5,7 @@ module CrystalWebApp
   Kemal.config.port = 3000
 
   get "/" do
+    # Render specified view (views/home.ecr) inside specified layout template (views/layouts/main.ecr)
     render "src/crystal_web_app/views/home.ecr", "src/crystal_web_app/views/layouts/main.ecr"
   end
 
